7e26bf64ca1648067b6392506ce3b16ff0b302a9
AngelicaMarie5 <angelica.cruz18@upr.edu>
2025-10-30 22:08:49 -0400

    Started MemoryCreate Function

---
+++ air_control_c/include/functions.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_FUNCTIONS_H_
+#define INCLUDE_FUNCTIONS_H_
+
+void MemoryCreate();
+void* TakeOffsFunction();
+
+#endif  // INCLUDE_FUNCTIONS_H_
--- air_control_c/src/functions.c
+++ air_control_c/src/functions.c
@@ -1,10 +1,50 @@
+#include "functions.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define SIZE sizeof(int) * 3
+
 int planes = 0;
 int takeoffs = 0;
 int total_takeoffs = 0;
 
+int* shared_PIDs;
+
 void MemoryCreate() {
   // TODO2: create the shared memory segment, configure it and store the PID of
   // the process in the first position of the memory block.
+
+  // Create shared memory object
+  /*
+  Not sure if it should be 0666 -> read and write acces
+  Or if it should be 0777 -> read, write and execute access
+  */
+  int shm_fd = shm_open("/SharedMemory", O_CREAT | O_RDWR, 0666);
+  if (shm_fd == -1) {
+    perror("shm_open failed");
+    exit(1);
+  }
+
+  // Configure size of shared memory object large enough to store three integer
+  // values
+  if (ftruncate(shm_fd, SIZE) == -1) {
+    perror("ftruncate failed");
+    exit(1);
+  }
+
+  shared_PIDs =
+      (int*)mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (shared_PIDs == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+
+  // Maybe?
+  shared_PIDs[0] = getppid();
 }
 
 // void SigHandler2(int signal) {}
